2d243fa6e8eeb1a8dcab1b3da536ea93385ddf59
yesolyo <ahnyelim77@gmail.com>
2023-08-14 22:23:59 +0900

    chore: 배포용 mixed content문제해결

--- trecord/src/pages/Auth/index.tsx
+++ trecord/src/pages/Auth/index.tsx
@@ -7,7 +7,7 @@ export const Auth = () => {
   const queryCode = url.searchParams.get('code');
 
   useEffect(() => {
-    console.log('로그인 요청');
+    console.log('로그인 요청 문제 확인');
     fetch(`${import.meta.env.VITE_BASE_URL}/api/v1/auth/google-login`, {
       method: 'POST',
       headers: {
