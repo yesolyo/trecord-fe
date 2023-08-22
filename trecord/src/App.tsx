67a93d7f2f87c606d4ec8f226c5aa8020593c7bc
seoyeon_jang <seoyeon_jang@tmax.co.kr>
2023-08-22 18:18:52 +0900

    :sparkles: [#15] query-client provider 추가

--- trecord/src/App.tsx
+++ trecord/src/App.tsx
@@ -2,13 +2,30 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/GlobalStyle';
 import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      suspense: true,
+    },
+    mutations: {
+      useErrorBoundary: true,
+    },
+  },
+});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Outlet />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Outlet />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
