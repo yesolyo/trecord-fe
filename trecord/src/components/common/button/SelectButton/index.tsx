1df62e3f3c61fc3bf4524d9313dc44ebedab9420
seoyeon_jang <seoyeon_jang@tmax.co.kr>
2023-08-25 15:06:17 +0900

    :sparkles: [#24] SelectButton 컴포넌트 구현

---
+++ trecord/src/components/common/button/SelectButton/index.tsx
@@ -0,0 +1,118 @@
+import { Icon } from '@components/common/Icon';
+import {
+  ReactElement,
+  useEffect,
+  useRef,
+  useState,
+  MouseEvent as ReactMouseEvent,
+} from 'react';
+import styled from 'styled-components';
+
+const StyledDropdown = styled.div`
+  min-width: 97px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--gray-100, #fff);
+  box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.12);
+
+  .option:not(:last-child) {
+    border-bottom: 1px solid #e9e9e9;
+  }
+
+  .option {
+    color: var(--gray-900, #1e1e1e);
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 20px; /* 142.857% */
+    padding: 9px 24px;
+    text-align: center;
+  }
+`;
+
+interface Props {
+  options: {
+    label: string;
+    value: string;
+  }[];
+  onSelect: (value: string) => void;
+}
+
+const Dropdown = ({ options, onSelect }: Props) => {
+  return (
+    <StyledDropdown>
+      {options.map((option) => (
+        <div
+          className="option"
+          key={option.value}
+          onClick={(e) => {
+            e.stopPropagation();
+            onSelect(option.value);
+          }}
+        >
+          {option.label}
+        </div>
+      ))}
+    </StyledDropdown>
+  );
+};
+
+const StyledDiv = styled.div`
+  position: 'relative';
+
+  > div {
+    position: absolute;
+    right: 10%;
+    z-index: 100;
+  }
+`;
+
+const SelectButton = ({ options, onSelect }: Props): ReactElement => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const handleClickButton = (event: ReactMouseEvent) => {
+    event.stopPropagation();
+    setDropdownVisible((prev) => !prev);
+  };
+
+  const handleSelect = (v: string) => {
+    onSelect(v);
+    setDropdownVisible(false);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setDropdownVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isDropdownVisible) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isDropdownVisible]);
+
+  return (
+    <StyledDiv onClick={handleClickButton}>
+      <Icon iconType="more" width={24} />
+      {isDropdownVisible && (
+        <div ref={dropdownRef}>
+          <Dropdown options={options} onSelect={handleSelect} />
+        </div>
+      )}
+    </StyledDiv>
+  );
+};
+
+export default SelectButton;
