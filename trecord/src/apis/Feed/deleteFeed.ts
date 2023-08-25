5e25391765ca6dae8700400404acd5b4fd9f1ec1
seoyeon_jang <seoyeon_jang@tmax.co.kr>
2023-08-26 00:33:39 +0900

    :sparkles: [#24] deleteFeed 에 피드들 불러오는 api 자동 재호출 추가

--- trecord/src/apis/Feed/deleteFeed.ts
+++ trecord/src/apis/Feed/deleteFeed.ts
@@ -1,6 +1,7 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { http } from '../_http';
+import FEED_API_KEY from './constants';
 
 interface Props {
   id: string;
@@ -15,9 +16,13 @@ const deleteFeed = async ({ id }: Props) => {
   return response;
 };
 
-/** @TODO 나중에 get도 수정되면 그 때 invaliation 과정 추가 */
 const useDeleteFeed = () => {
+  const queryClient = useQueryClient();
+
   return useMutation(deleteFeed, {
+    onSuccess: () => {
+      queryClient.invalidateQueries([FEED_API_KEY.FEEDS]);
+    },
     /** @TODO 나중에 error boundary 추가 */
     onError: (e) => console.log(e),
   });
