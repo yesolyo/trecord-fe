5a2385397cbfddee8f2bdd72cb6d429e56f718b9
seoyeon_jang <seoyeon_jang@tmax.co.kr>
2023-10-06 10:33:42 +0900

    :sparkles: [#67] feed pagination 적용

--- trecord/src/apis/Feed/getFeeds.ts
+++ trecord/src/apis/Feed/getFeeds.ts
@@ -3,24 +3,22 @@ import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { http } from '../_http';
 import FEED_API_KEY from './constants';
 import { Feed } from '@/types/feeds';
+
 interface Props {
-  pageCount: number;
+  page: number;
 }
-const getFeeds = async ({ pageCount }: Props): Promise<Page<Feed>> => {
-  const url = `/api/v1/feeds?page=0&size=${pageCount}`;
+
+const getFeeds = async ({ page }: Props): Promise<Page<Feed>> => {
+  const url = `/api/v1/feeds?page=${page}&size=5`;
   const response: Page<Feed> = await http.authGet(url);
 
   return response;
 };
 
-const useGetFeeds = ({ pageCount }: Props): UseQueryResult<Page<Feed>> => {
-  return useQuery(
-    [FEED_API_KEY.FEEDS, { pageCount }],
-    () => getFeeds({ pageCount }),
-    {
-      suspense: true,
-    },
-  );
+const useGetFeeds = ({ page }: Props): UseQueryResult<Page<Feed>> => {
+  return useQuery([FEED_API_KEY.FEEDS, { page }], () => getFeeds({ page }), {
+    suspense: true,
+  });
 };
 
 export default useGetFeeds;
